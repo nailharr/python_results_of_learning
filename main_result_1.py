15b3954659dade3e9e19bd8d8c041fbeb74d439f
nailharr <nailharr@gmail.com>
2023-07-26 13:40:13 +0300

    rename file

---
+++ main_result_1.py
@@ -0,0 +1,19 @@
+from user_requests import *
+from messages import *
+
+a = 'ДА!'
+b = a
+c = b
+res = c
+
+while True:
+    choice = select_choice()
+    if choice == 1:
+        print(msg(choice, res))
+    elif choice == 2:
+        print(msg(choice, res))
+    elif choice == 3:
+        print(msg(choice, res))
+    else:
+        print(msg_wrong())
+
