1b0ee8bd990cb731b2fb064ef1c0c1eca42895c2
nailharr <nailharr@gmail.com>
2023-07-26 13:49:00 +0300

    fix variables

--- main_result_1.py
+++ main_result_1.py
@@ -4,15 +4,14 @@ from messages import *
 a = 'ДА!'
 b = a
 c = b
-res = c
 
 while True:
     choice = select_choice()
     if choice == 1:
-        print(msg(choice, res))
+        print(msg(choice, a))
     elif choice == 2:
-        print(msg(choice, res))
+        print(msg(choice, b))
     elif choice == 3:
-        print(msg(choice, res))
+        print(msg(choice, c))
     else:
         print(msg_wrong())
