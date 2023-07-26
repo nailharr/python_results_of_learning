227c856a0e812e99a37056cb4ea9e89285acc578
nailharr <nailharr@gmail.com>
2023-07-26 13:35:34 +0300

    123

---
+++ messages.py
@@ -0,0 +1,8 @@
+def msg(i, j):
+    print(f"Ваш выбор: {i}")
+    return f"Результат: {j}\n-------------"
+
+
+def msg_wrong():
+    return f"Жми на ссылку, потом повтори выбор:\nhttps://natribu.org/"
+#
---
+++ user_requests.py
@@ -0,0 +1,6 @@
+def select_choice():
+    ask = int(input("Будете пить сегодня?\n"
+                    "Введите 1, 2 или 3: \n"))
+    return ask
+
+#
